c1872f68443d9e311106056d2045ccacb76e82a3
Tyler.Martinez <tyler.martinez@respec.com>
2023-08-14 08:24:22 -0500

    [Navbar] Add mobile friendly ui elements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,41 +13,52 @@ export default function Navbar({ catagory }: { catagory: string | undefined }) {
   };
 
   return (
-    <div className="grid grid-cols-3 grid-rows-1 sticky top-0 w-100 px-8 bg-slate-700 text-white">
-      <div className="flex items-center justify-start gap-x-4">
-        <button className="h-100 hover:bg-slate-600 p-4">
-          <Bars3Icon className="h-12 w-12" />
-        </button>
-        <div className="flex gap-x-2 items-center justify-center">
-          <label className="text text-center">Bulk Print</label>
-          <Switch
-            checked={enabled}
-            onChange={handleBulkPrintRequestChange}
-            className={`${enabled ? "bg-emerald-400" : "bg-slate-900"}
+    <div className="grid grid-cols-1 grid-rows-auto">
+      <div className="grid grid-cols-3 grid-rows-1 sticky top-0 w-100 px-2 sm:px-4 md:px-8 bg-slate-700 text-white">
+        <div className="flex items-center justify-start gap-x-1 sm:gap-x-2 md:gap-x-4">
+          <button className="h-100 hover:bg-slate-600 p-4">
+            <Bars3Icon className="h-12 w-12" />
+          </button>
+          <div className="hidden md:flex gap-x-2 items-center justify-center">
+            <label className="text-center">Bulk Print</label>
+            <Switch
+              checked={enabled}
+              onChange={handleBulkPrintRequestChange}
+              className={`${enabled ? "bg-emerald-400" : "bg-slate-900"}
           relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span className="sr-only">Bulk Print Toggle</span>
-            <span
-              aria-hidden="true"
-              className={`${enabled ? "translate-x-9" : "translate-x-0"}
+            >
+              <span className="sr-only">Bulk Print Toggle</span>
+              <span
+                aria-hidden="true"
+                className={`${enabled ? "translate-x-9" : "translate-x-0"}
             pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-            />
-          </Switch>
+              />
+            </Switch>
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {catagory != undefined ? (
+            <h1 className="hidden sm:visible sm:text-2xl md:text-3xl font-bold select-none">
+              {catagory}
+            </h1>
+          ) : null}
+        </div>
+        <div className="flex items-center justify-end gap-x-1 sm:gap-x-2 md:gap-x-4">
+          <button className="h-100 hover:bg-slate-600 p-4">
+            <PrinterIcon className="h-12 w-12" />
+          </button>
+          <button className="h-100 hover:bg-slate-600 p-4">
+            <UserCircleIcon className="h-12 w-12" />
+          </button>
         </div>
       </div>
-      <div className="flex items-center justify-center">
-        {catagory != undefined ? (
-          <h1 className="text-3xl font-bold select-none">{catagory}</h1>
-        ) : null}
-      </div>
-      <div className="flex items-center justify-end gap-x-4">
-        <button className="h-100 hover:bg-slate-600 p-4">
-          <PrinterIcon className="h-12 w-12" />
-        </button>
-        <button className="h-100 hover:bg-slate-600 p-4">
-          <UserCircleIcon className="h-12 w-12" />
-        </button>
-      </div>
+      {catagory != undefined ? (
+        <div className="bg-slate-900 md:hidden py-1">
+          <h1 className="text-white text-center text-3xl font-bold select-none">
+            {catagory}
+          </h1>
+        </div>
+      ) : null}
     </div>
   );
 }
