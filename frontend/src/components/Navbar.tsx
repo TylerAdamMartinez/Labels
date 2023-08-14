dad15715eebb9ef978faa663e631ed4822696932
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2023-08-13 21:01:28 -0500

    [/src] Update Home page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,15 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { PrinterIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+
 export default function Navbar() {
-    return <>NavBar</>;
+    return (
+    <div className="flex sticky top-0 w-100 py-2 px-6 bg-slate-700 text-white">
+        <Bars3Icon className="h-6 w-6"/>
+
+        <div className="flex">
+            <PrinterIcon className="h-6 w-6"/>
+            <UserCircleIcon className="h-6 w-6"/>
+        </div>
+    </div>
+    );
 }
